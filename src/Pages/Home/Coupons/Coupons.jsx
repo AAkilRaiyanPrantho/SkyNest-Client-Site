cf929d1baa3a31f2b7582251ab0a8a917de36c5d
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2023-11-26 21:39:52 +0600

    Coupon Section added

---
+++ src/Pages/Home/Coupons/Coupons.jsx
@@ -0,0 +1,69 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+import { FreeMode, Pagination } from "swiper/modules";
+
+const Coupons = () => {
+  return (
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={30}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[FreeMode, Pagination]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <div className="card w-96 bg-primary text-primary-content">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="card w-96 bg-primary text-primary-content">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="card w-96 bg-primary text-primary-content">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="card w-96 bg-primary text-primary-content">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Coupons;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet";
 import Banner from "../Banner/Banner";
 import Headings from "../../Shared/Headings/Headings";
+import Coupons from "../Coupons/Coupons";
 
 
 const Home = () => {
@@ -11,7 +12,11 @@ const Home = () => {
       </Helmet>
             <h2>This is HOME</h2>
             <Banner></Banner>
+            <Headings mainHeading={'Flash Deals'} subHeading={'Get the best deals'}></Headings>
+            <Coupons></Coupons>
             <Headings mainHeading={'About'} subHeading={'Know about our building'}></Headings>
+            <Headings mainHeading={'Where to Find Us?'} subHeading={'Map View of our location'}></Headings>
+            
         </div>
     );
 };
